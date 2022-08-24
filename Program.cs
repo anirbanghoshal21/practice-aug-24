7aa5ddb0c3b2992399a3f97bf8540b3f3c1e1264
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 21:40:51 +0530

    problem 7

--- Program.cs
+++ Program.cs
@@ -4,36 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Q1
+namespace Q6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string str_equ1 = "Dot net compare";
-
-            string str_equ2 = " .net compare";
-
-
-
-            if (String.Equals(str_equ1, str_equ2))
-
-            {
-
-                Console.WriteLine("Both Strings are equal!");
-
-            }
-
-            else
-
-            {
-
-                Console.WriteLine("Both Strings are not equal!");
-
-            }
-
-
-
+            DateTime dt1 = new DateTime();
+            dt1 = DateTime.Now;
+            Console.WriteLine(dt1);
+            Console.WriteLine("Day of year "  + dt1.DayOfYear);
+            Console.WriteLine("Month=  " + dt1.Month);
+            Console.WriteLine("Day=  " + dt1.Day);
+            Console.WriteLine("Hour = " + dt1.Hour);
+            Console.WriteLine("Minute = " + dt1.Minute);
+            Console.WriteLine("Second = " + dt1.Second);
+            Console.WriteLine("Millisecond = " + dt1.Millisecond);
             Console.ReadLine();
         }
     }
