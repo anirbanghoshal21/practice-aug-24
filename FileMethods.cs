1b5da8fabab0ab1d6368a0a73205f1e7bc3a6ca9
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-25 00:07:21 +0530

    problem 6

---
+++ FileMethods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ReusableCodeLibrary
+{
+    public class FileMethods
+    {
+        public static void MakeFile()
+        {
+            string path = @"C:\\Users\\Public\\Anirban\\FilesHandling\firstfile.txt";
+            File.Create(path);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,38 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Q1
+using LogicOfTheApp;
+namespace FilesHandling
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string str_equ1 = "Dot net compare";
-
-            string str_equ2 = " .net compare";
-
-
-
-            if (String.Equals(str_equ1, str_equ2))
-
-            {
-
-                Console.WriteLine("Both Strings are equal!");
-
-            }
-
-            else
-
-            {
-
-                Console.WriteLine("Both Strings are not equal!");
-
-            }
-
-
-
-            Console.ReadLine();
+            SampleClass.Hello();
+            Console.Read();
         }
     }
 }
