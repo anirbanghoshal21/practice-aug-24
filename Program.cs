5633f8fffb7cdcd65dc3f4ba7129dee385e0cb05
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-24 20:51:51 +0530

    problem 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str_equ1 = "Dot net compare";
+
+            string str_equ2 = " .net compare";
+
+
+
+            if (String.Equals(str_equ1, str_equ2))
+
+            {
+
+                Console.WriteLine("Both Strings are equal!");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Both Strings are not equal!");
+
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
