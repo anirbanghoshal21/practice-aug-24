fd24306793d303570d2f220a498c56cb09f9c8e0
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-08-25 00:09:12 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReusableCodeLibrary;
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SampleClass.Printsting();
+            FileMethods.MakeFile();
+            Console.Read();
+        }
+    }
+}
